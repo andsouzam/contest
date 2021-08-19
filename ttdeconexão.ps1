15fe9c1b2129af712887311f6d3c9aeccd584d36
andsouzam <anderson.dsmiranda@gmail.com>
2021-08-19 15:58:27 -0300

    up de script melhorado

--- ttdeconexão.ps1
+++ ttdeconexão.ps1
@@ -1,14 +1,16 @@
 function testederede {
-    $a= (Get-Date).ToString('dddd, dd "de" MMMM "de" yyyy "as" HH:mm:ss tt')
+     $a= (Get-Date).ToString('dddd, dd "de" MMMM "de" yyyy "as" HH:mm:ss tt')
     if (Test-Connection amazon.com -ErrorAction SilentlyContinue ){ 
-    write-host "TÁ FUNFANDO -- $a"  -ForegroundColor white -BackgroundColor Green
-    Start-Sleep -Seconds 20
-    testederede}
+        Write-host "TÁ FUNFANDO -- $a"  -ForegroundColor white -BackgroundColor Green
+        Start-Sleep -Seconds 20
+        testederede}
     
-    else { Write-Host "CAIIUUU -- $a" -ForegroundColor Black -BackgroundColor Red
-    sleep -Seconds 10
-    testederede
-    }
-    }
+    else { 
+        Write-Host "CAIIUUU -- $a" -ForegroundColor Black -BackgroundColor Red
+        Start-Sleep -Seconds 10
+        testederede}
     
-    testederede
+}
+testederede
+
+
