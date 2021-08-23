7d8ef2f146a6c8fdacefa63cd222a83cfbe527e1
AndSouzaM <anderson.dsmiranda@gmail.com>
2021-08-23 15:57:07 -0300

    add script completo

---
+++ TESTE DE INTERNET.ps1
@@ -0,0 +1,51 @@
+﻿function testederede {
+
+    $a= (Get-Date).ToString('dddd, dd "de" MMMM "de" yyyy "as" HH:mm:ss tt')
+
+
+    if (Test-Connection amazon.com -ErrorAction SilentlyContinue ){ 
+        write-host "TÁ FUNFANDO -- $a"  -ForegroundColor white -BackgroundColor Green
+        start-sleep -Seconds 20
+        testederede}
+
+    else {Write-Host "CAIIUUU -- $a" -ForegroundColor Black -BackgroundColor Red
+          start-sleep -Seconds 10
+          testederede2}
+}
+
+function testederede2 {
+    if (Test-Connection amazon.com -ErrorAction SilentlyContinue ){ 
+        write-host "TÁ FUNFANDO -- $a"  -ForegroundColor white -BackgroundColor Green
+        start-sleep -Seconds 20
+        testederede}
+
+else {
+    Write-Host "CAIIUUU tentando resolver... -- $a" -ForegroundColor Black -BackgroundColor Red
+    #comando cmd para resolver o problema
+    Start-Sleep -Seconds 10
+    testederede3}
+}
+
+function testederede3 {
+if (Test-Connection amazon.com -ErrorAction SilentlyContinue ){ 
+write-host "TÁ FUNFANDO -- $a"  -ForegroundColor white -BackgroundColor Green
+start-sleep -Seconds 20
+testederede}
+
+else { Write-Host "CAIIUUU -- $a" -ForegroundColor Black -BackgroundColor Red
+start-sleep -Seconds 10
+testederede4}
+}
+
+function testederede4 {
+if (Test-Connection amazon.com -ErrorAction SilentlyContinue ){ 
+write-host "TÁ FUNFANDO -- $a"  -ForegroundColor white -BackgroundColor Green
+start-sleep -Seconds 20
+testederede}
+
+else { Write-Host "CAIIUUU nao conseguimos resolver-- $a" -ForegroundColor Black -BackgroundColor Red
+start-sleep -Seconds 10
+testederede3}
+}
+
+testederede
