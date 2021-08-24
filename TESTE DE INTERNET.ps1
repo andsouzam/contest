14d4ac3d865b543eaffb93045441f397b6ce3e9d
andsouzam <anderson.dsmiranda@gmail.com>
2021-08-24 15:08:54 -0300

    ajuste em print de horários

--- TESTE DE INTERNET.ps1
+++ TESTE DE INTERNET.ps1
@@ -14,36 +14,39 @@
 }
 
 function testederede2 {
+    $b= (Get-Date).ToString('dddd, dd "de" MMMM "de" yyyy "as" HH:mm:ss tt')
     if (Test-Connection globo.com -ErrorAction SilentlyContinue ){ 
-        write-host "TÁ FUNFANDO -- $a"  -ForegroundColor white -BackgroundColor Green
+        write-host "TÁ FUNFANDO -- $b"  -ForegroundColor white -BackgroundColor Green
         start-sleep -Seconds 10
         testederede}
 
     else {
-        Write-Host "CAIIUUU -- $a" -ForegroundColor Black -BackgroundColor Red
+        Write-Host "CAIIUUU -- $b" -ForegroundColor Black -BackgroundColor Red
         #comando cmd para resolver o problema
         Start-Sleep -Seconds 10
         testederede3}
 }
 
 function testederede3 {
+    $c= (Get-Date).ToString('dddd, dd "de" MMMM "de" yyyy "as" HH:mm:ss tt')
     if (Test-Connection globo.com -ErrorAction SilentlyContinue ){ 
-        write-host "TÁ FUNFANDO -- $a"  -ForegroundColor white -BackgroundColor Green
+        write-host "TÁ FUNFANDO -- $c"  -ForegroundColor white -BackgroundColor Green
         start-sleep -Seconds 10
         testederede}
 
-    else { Write-Host "CAIIUUU -- $a" -ForegroundColor Black -BackgroundColor Red
+    else { Write-Host "CAIIUUU -- $c" -ForegroundColor Black -BackgroundColor Red
         start-sleep -Seconds 10
         testederede4}
 }
 
 function testederede4 {
+    $d= (Get-Date).ToString('dddd, dd "de" MMMM "de" yyyy "as" HH:mm:ss tt')
     if (Test-Connection amazon.com -ErrorAction SilentlyContinue ){ 
-        write-host "TÁ FUNFANDO -- $a"  -ForegroundColor white -BackgroundColor Green
+        write-host "TÁ FUNFANDO -- $d"  -ForegroundColor white -BackgroundColor Green
         start-sleep -Seconds 10
         testederede}
 
-    else { Write-Host "CAIIUUU nao conseguimos resolver-- $a" -ForegroundColor Black -BackgroundColor Red
+    else { Write-Host "CAIIUUU nao conseguimos resolver-- $d" -ForegroundColor Black -BackgroundColor Red
         start-sleep -Seconds 10
         testederede3}
 }
